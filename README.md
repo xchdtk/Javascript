4236e5b3c513b0c2bcf11b92e2e983ed7221ff11
kimjinsoo <1004kjs_@naver.com>
2021-07-21 23:29:38 +0900

    velopert 폴더 생성

--- README.md
+++
@@ -1,2 +0,0 @@
-# Javascript
-Javascript 공부한 것을 기록합니다
---
+++ velopert/비동기 처리/index.js
@@ -0,0 +1,8 @@
+function work() {
+  const start = Date.now();
+  for (let i = 0; i < 1000000000; i++) {}
+  const end = Date.now();
+  console.log(end - start + "ms");
+}
+
+work();
